a44725b0f8f1a0d25e559dfdea988d7c9af17ec0
LarisaGl <89888985265@mail.ru>
2018-06-28 00:12:05 +0300

    +table

--- about.php
+++ about.php
@@ -3,11 +3,15 @@
   <head>
     <title>about</title>
     <meta charset="UTF-8">
+    <style>
+   		table, tr, td { 
+    	border: 1px solid black;
+    	text-align: center;
+   		}
+ 	</style>
   </head>
   
   <body>
-    <h1>Страница пользователя Лариса Головина</h1>
-
     <?php
     $userName='Лариса';
     $userSurname='Головина';
@@ -15,14 +19,35 @@
     $userMale='89888985265@mail.ru';
     $userAddress='Ростов-на-Дону';
     $userAbout='I am Groot';
-    
-    echo 'Имя: '.$userName.'<br>';
-    echo 'Фамилия: '.$userSurname.'<br>';
-    echo 'Возраст: '.$userAge.'<br>';
-    echo 'Адрес электронной почты: '.$userMale.'<br>';
-    echo 'Город: '.$userAddress.'<br>';
-    echo 'О себе: '.$userAbout;
     ?>
+
+    <h1>Страница пользователя Лариса Головина</h1>
+    <table>
+    	<tr>
+    		<td>Имя:</td>
+    		<td><?= $userName ?></td>
+    	</tr>
+    	<tr>
+    		<td>Фамилия:</td>
+    		<td><?= $userSurname ?></td>
+    	</tr>
+    	<tr>
+    		<td>Возраст:</td>
+    		<td><?= $userAge ?></td>
+    	</tr>
+    	<tr>
+    		<td>Адрес электронной почты:</td>
+    		<td><?= $userMale ?></td>
+    	</tr>
+    	<tr>
+    		<td>Город:</td>
+    		<td><?= $userAddress ?></td>
+    	</tr>
+    	<tr>
+    		<td>О себе:</td>
+    		<td><?= $userAbout ?></td>
+    	</tr>
+    </table>    
 
   </body>
 </html>
