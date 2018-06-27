531efc48ae5d7060a131368c37a1308bb2f0f48e
LarisaGl <89888985265@mail.ru>
2018-06-27 15:20:13 +0300

    new

--- about.php
+++ about.php
@@ -8,26 +8,21 @@
   <body>
     <h1>Страница пользователя Лариса Головина</h1>
 
-      <?php
-      $userName='Лариса';
-      $userSurname='Головина';
-      $userAge=26;
-      $userMale='89888985265@mail.ru';
-      $userAddress='Ростов-на-Дону';
-      $userAbout='I am Groot';
-      ?>
-      
-      <?php echo 'Имя:' . ' ' .  $userName;?>
-      <br>
-      <?php echo 'Фамилия:' . ' ' . $userSurname;?>
-      <br>
-      <?php echo 'Возраст:' . ' ' . $userAge;?>
-      <br>
-      <?php echo 'Адрес электронной почты:' . ' ' . $userMale;?>
-      <br>
-      <?php echo 'Город:' . ' ' . $userAddress;?>
-      <br>
-      <?php echo 'О себе:' . ' ' . $userAbout;?>
+    <?php
+    $userName='Лариса';
+    $userSurname='Головина';
+    $userAge=26;
+    $userMale='89888985265@mail.ru';
+    $userAddress='Ростов-на-Дону';
+    $userAbout='I am Groot';
+    
+    echo 'Имя: '.$userName.'<br>';
+    echo 'Фамилия: '.$userSurname.'<br>';
+    echo 'Возраст: '.$userAge.'<br>';
+    echo 'Адрес электронной почты: '.$userMale.'<br>';
+    echo 'Город: '.$userAddress.'<br>';
+    echo 'О себе: '.$userAbout;
+    ?>
 
   </body>
 </html>
